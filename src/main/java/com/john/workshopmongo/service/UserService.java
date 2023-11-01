3c4b22e0bdb2a9d386f376c8a4649e69f53cbb11
John Costa <jooohn.elvis@hotmail.com>
2023-11-01 00:06:41 -0300

    Inserção de usuário com POST

--- src/main/java/com/john/workshopmongo/service/UserService.java
+++ src/main/java/com/john/workshopmongo/service/UserService.java
@@ -5,6 +5,7 @@ import com.john.workshopmongo.repository.UserRepository;
 import com.john.workshopmongo.service.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import com.john.workshopmongo.dto.UserDTO;
 
 import java.util.List;
 import java.util.Optional;
@@ -21,5 +22,13 @@ public class UserService {
     public User findById(String id) {
         Optional<User> obj = repo.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public User insert(User obj) {
+        return repo.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDTO) {
+        return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
     }
 }
